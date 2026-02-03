135cdc127a5fb9c85e1ae4a1cfe421283de7827b
Sebastianrk18 <20242397@itla.edu.do>
2026-02-03 13:18:51 -0400

    Add files via upload

---
+++ SQLQueryETL.sql
@@ -0,0 +1,45 @@
+CREATE DB_Ventas_ETL;
+Go
+
+USE DB_Ventas_ETL;
+GO
+
+
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    Phone VARCHAR(50),
+    City VARCHAR(50),
+    Country VARCHAR(50)
+);
+
+
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY,
+    ProductName VARCHAR(100),
+    Category VARCHAR(50),
+    Price DECIMAL(10, 2),
+    Stock INT
+);
+
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerID INT,
+    OrderDate DATE,
+    Status VARCHAR(20),
+    CONSTRAINT FK_Customer FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+);
+
+
+CREATE TABLE OrderDetails (
+    OrderID INT,
+    ProductID INT,
+    Quantity INT,
+    TotalPrice DECIMAL(10, 2),
+    PRIMARY KEY (OrderID, ProductID), 
+    CONSTRAINT FK_Order FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
+    CONSTRAINT FK_Product FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+);
+
